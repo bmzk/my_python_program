2d5d27e81174a6b4e0fe0c8ff2da51c98e228e1a
Fu <bmzk123@163.com>
2019-07-10 08:05:49 +0800

    0

--- 移动文件/移动文件.py
+++ 移动文件/移动文件.py
@@ -1,5 +1,27 @@
 '''如果文件名包含 指定字符,则将其移动到上一级目录中'''
 mystr=''
 
-path="D:\\KwDownload"
+mypath="D:\\KwDownload"
 
+import os
+
+os.chdir(path)
+os.getcwd()
+
+os.path.dirname(path)
+
+
+def get_all_files(mydir):
+    '''python 获取文件夹下所有文件包含子目录文件'''
+    files_ = []
+    mylist = os.listdir(mydir)
+    for i in range(0, len(mylist)):
+        path = os.path.join(mydir, list[i])
+        if os.path.isdir(path):
+            files_.extend(get_all_files(path))
+        if os.path.isfile(path):
+            files_.append(path)
+    return files_
+
+get_all_files(mypath)
+mydir="D:\\KwDownload"
