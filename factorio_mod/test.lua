17bf0af9dbe3b392a6d19320fbfbe048d1c2b16f
bmzk <37799175+bmzk@users.noreply.github.com>
2019-12-21 12:16:55 +0800

    0

--- factorio_mod/test.lua
+++ factorio_mod/test.lua
@@ -2,10 +2,12 @@ a={"ladsd",455555555555555555555555}
 
 print(a)
 print(type(a))
---b={ee=5,dd={"as",5},ff={"wqe",456}}
+#--b={ee=5,dd={"as",5},ff={"wqe",456}}
 b={}
 print(type(b))
 print(b)
 b[1]={"e",8888}
 print(type(b))
 print(b)
+for i in range(9):
+    print(i,a)
